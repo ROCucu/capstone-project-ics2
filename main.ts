d3b14c7e0b1894234ab17368b10a62515d56a33e
ROCucu <michael.strimbu@gmail.com>
2025-06-04 23:06:31 -0700

    Added Attacks, And Fixed bugs

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const Boss = SpriteKind.create()
+    export const orbs = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`Can`, mySprite, 0, -100)
@@ -9,16 +10,40 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Boss, function (sprite, otherSprite: Boss) {
     otherSprite.hit(1)
-    sprites.destroy(sprite)
+sprites.destroy(sprite)
+})
+sprites.onDestroyed(SpriteKind.Boss, function (sprite) {
+    info.changeScoreBy(10)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tilePath5, function (sprite, location) {
     tiles.placeOnRandomTile(mySprite, sprites.builtin.forestTiles0)
     speed += 5
 })
+scene.onHitWall(SpriteKind.orbs, function (sprite, location) {
+    sprites.destroy(sprite)
+})
+info.onLifeZero(function () {
+    game.gameOver(false)
+    game.setGameOverScoringType(game.ScoringType.HighScore)
+})
+sprites.onOverlap(SpriteKind.orbs, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.orbs, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    sprites.destroy(otherSprite)
+})
+scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
+    sprites.destroy(sprite)
+})
+let mySprite2: Sprite = null
 let projectile: Sprite = null
 let mySprite: Sprite = null
+let Boss_Checker = 1
 let speed = 60
 mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
+info.setLife(3)
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.setBackgroundColor(9)
 scene.cameraFollowSprite(mySprite)
@@ -42,4 +67,31 @@ game.onUpdateInterval(10, function () {
     mySprite.vy += 3
     mySprite.vx = speed
     boss.setPosition(mySprite.x, mySprite.y - 60)
+    info.changeScoreBy(0.001)
+})
+game.onUpdateInterval(3000, function () {
+    if (Boss_Checker == 1) {
+        for (let index = 0; index < 3; index++) {
+            mySprite2 = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . 2 2 2 . 2 2 2 . . . . . . 
+                . 2 2 2 2 2 2 2 . 2 2 . . . . . 
+                . 2 2 . 2 2 2 2 2 2 2 . . . . . 
+                . 2 . . 2 2 2 2 2 2 2 . . . . . 
+                2 2 . 2 2 2 . 2 2 2 2 . . . . . 
+                2 . 2 2 2 2 . 2 2 . 2 . . . . . 
+                2 . 2 2 2 2 2 2 2 2 . . . . . . 
+                . 2 2 2 2 2 2 2 2 . . . . . . . 
+                . . 2 2 2 2 2 2 2 . . . . . . . 
+                . . 2 2 2 2 2 2 . . . . . . . . 
+                . . . 2 . 2 . . . . . . . . . . 
+                . . . . 2 . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.orbs)
+            mySprite2.setPosition(boss.x, boss.y)
+            mySprite2.setVelocity(randint(-10, 10), randint(50, 70))
+        }
+    }
 })
