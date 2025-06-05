5e1d0b7875c4dfd99689a45d42fbd8cfeb5200e0
ROCucu <michael.strimbu@gmail.com>
2025-06-04 22:53:33 -0700

    Added Boss, Boss Class, projectile damage

--- main.ts
+++ main.ts
@@ -1,31 +1,45 @@
+namespace SpriteKind {
+    export const Boss = SpriteKind.create()
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(assets.image`Can`, mySprite, 0, -100)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.vy += -80
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Boss, function (sprite, otherSprite: Boss) {
+    otherSprite.hit(1)
+    sprites.destroy(sprite)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.castle.tilePath5, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, sprites.builtin.forestTiles0)
+    speed += 5
+})
+let projectile: Sprite = null
 let mySprite: Sprite = null
-mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
+let speed = 60
+mySprite = sprites.create(assets.image`Player`, SpriteKind.Player)
 tiles.setCurrentTilemap(tilemap`level1`)
-mySprite.setVelocity(100, 0)
 scene.setBackgroundColor(9)
 scene.cameraFollowSprite(mySprite)
 tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 17))
+class Boss extends sprites.ExtendableSprite{
+    health = 10
+
+    hit(damage: number): void {
+        this.health -= damage
+        if  (this.health <= 0){
+            sprites.destroy(boss)
+        }
+    }
+    constructor(image: Image, SpriteKind: number){
+        super(image,SpriteKind)
+        this.health = 10
+    }
+}
+let boss = new Boss(assets.image`x`,SpriteKind.Boss)
 game.onUpdateInterval(10, function () {
     mySprite.vy += 3
-    mySprite.vx = 60
+    mySprite.vx = speed
+    boss.setPosition(mySprite.x, mySprite.y - 60)
 })
