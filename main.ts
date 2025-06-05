00c1acc18156cde6b13d90f58c33565a13f2f4b5
ROCucu <michael.strimbu@gmail.com>
2025-06-04 22:33:08 -0700

    Tilemap Added - Update 0.1

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += -80
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite.setVelocity(100, 0)
+scene.setBackgroundColor(9)
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 17))
+game.onUpdateInterval(10, function () {
+    mySprite.vy += 3
+    mySprite.vx = 60
+})
